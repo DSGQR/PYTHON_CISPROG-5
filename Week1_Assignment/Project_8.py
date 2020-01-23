abb3b2614f55fa8a910f012e8410cc0ff5e0fbd8
DSGQR <amini.daniel@gmail.com>
2020-01-22 18:31:15 -0800

    Add Update for Projects 8 & 9

--- Week1_Assignment/Project_8.py
+++ Week1_Assignment/Project_8.py
@@ -1,3 +1,14 @@
 num1 = input('Enter a number: ')
 num2 = num1 + 1
 print(num2)
+
+### ------ Output ------ ###
+# Traceback (most recent call last):
+#   File "main.py", line 2, in <module>
+#     num2 = num1 + 1
+# TypeError: can only concatenate str (not "int") to str
+
+### ------ Explinaiton ------ ###
+# I received a type error. The program thinks I am trying to cancatenate 
+# two different data types. Cancatenation can only be done with strings and
+# not inters. 
--- Week1_Assignment/Project_9.py
+++ Week1_Assignment/Project_9.py
@@ -1,2 +1,6 @@
 firstName = input('Enter your first name: ')
 print(firstName)
+
+### ------ Output ------ ###
+# In the Shell there is no output however when I use the print function 
+# it prints the name entered
